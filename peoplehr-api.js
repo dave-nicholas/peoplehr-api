fb083647c844614eeb89aa792eb56f57e31ca53e
Dave Nicholas <davenicholasuk@gmail.com>
2018-01-21 13:26:02 +0000

    Add query

--- peoplehr-api.js
+++ peoplehr-api.js
@@ -1,46 +1,56 @@
 'use strict';
 
-const fetch = require('node-fetch');
-
 const url = 'https://api.peoplehr.net';
 
-let userApiKey = '';
+let APIKey = '';
+let fetch;
 
 class PeopleHR {
   /**
    * Create an instance
    *
-   * @param {string} apiKey apikey obtained from the peoplehr web admin
+   * @param {string} apiKey - obtained from the peoplehr web admin
+   * @param {(function|Object)} _fetch - optional use to override fetch
    */
-  constructor(apiKey) {
+  constructor(apiKey, _fetch = null) {
     if (apiKey && typeof apiKey === 'string') {
-      userApiKey = apiKey;
+      APIKey = apiKey;
     } else if (process.env.hasOwnProperty('PEOPLEHR_API_KEY')) {
-      userApiKey = process.env.PEOPLEHR_API_KEY;
+      APIKey = process.env.PEOPLEHR_API_KEY;
     } else if (process.env.hasOwnProperty('REACT_APP_PEOPLEHR_API_KEY')) {
-      userApiKey = process.env.REACT_APP_PEOPLEHR_API_KEY;
+      APIKey = process.env.REACT_APP_PEOPLEHR_API_KEY;
     } else {
       throw new Error('No apiKey set, see readme.');
     }
+
+    fetch =
+      _fetch && typeof _fetch === 'function' ? _fetch : require('node-fetch');
   }
 
   /**
-   * Returns a JSON response
+   * Returns a Promise
    *
    * @param {string} endpoint
    * @param {string} action
    * @param {Object} params
+   * @returns {Promise} JSON object
    */
-  get(endpoint, action, params) {}
-
-  /**
-   * Returns a
-   *
-   * @param {string} endpoint
-   * @param {string} action
-   * @param {Object} payload
-   */
-  post(endpoint, action, payload) {}
+  async query(endpoint, action, params = {}) {
+    try {
+      const response = await fetch(`${url}/${endpoint}`, {
+        method: 'POST',
+        body: JSON.stringify({
+          ...params,
+          Action: action,
+          APIKey
+        })
+      });
+      const json = await response.json();
+      return json;
+    } catch (error) {
+      return error;
+    }
+  }
 }
 
 module.exports = PeopleHR;
--- tests/peoplehr.spec.js
+++ tests/peoplehr.spec.js
@@ -1,22 +1,42 @@
 'use strict';
 
+const fetchMock = require('fetch-mock');
 const PeopleHR = require('../peoplehr-api').default;
 
 const apiKey = 'THIS_IS_A_TEST_KEY';
 
+process.env.test = true;
+
 test('Can instanstiate library with apiKey parameter', () => {
-  const peoplerhr = new PeopleHR(apiKey);
-  expect(peoplerhr).toBeInstanceOf(PeopleHR);
+  const phr = new PeopleHR(apiKey);
+  expect(phr).toBeInstanceOf(PeopleHR);
 });
 
 test('Can instanstiate library with process.env.PEOPLEHR_API_KEY', () => {
   process.env.PEOPLEHR_API_KEY = apiKey;
-  const peoplerhr = new PeopleHR();
-  expect(peoplerhr).toBeInstanceOf(PeopleHR);
+  const phr = new PeopleHR();
+  expect(phr).toBeInstanceOf(PeopleHR);
 });
 
 test('Can instanstiate library with process.env.REACT_APP_PEOPLEHR_API_KEY', () => {
   process.env.REACT_APP_PEOPLEHR_API_KEY = apiKey;
-  const peoplerhr = new PeopleHR();
-  expect(peoplerhr).toBeInstanceOf(PeopleHR);
+  const phr = new PeopleHR();
+  expect(phr).toBeInstanceOf(PeopleHR);
+});
+
+test('Can call query', async () => {
+  fetchMock.mock('*', {
+    isError: false,
+    Status: 0,
+    Message: 'The request processed successfully.',
+    Result: [{ EmployeeId: '123' }]
+  });
+
+  const phr = new PeopleHR(apiKey, fetchMock.sandbox());
+  const response = await phr.query('Employee', 'GetAllEmployeeDetail', {
+    IncludeLeavers: false
+  });
+
+  expect(response.isError).toBe(false);
+  expect(response.Result[0].EmployeeId).toBe('123');
 });
